1da4ed0d04f7c60ef4508f2219348e53b70cc6dc
Debjit Biswas <debjit012@gmail.com>
2022-02-19 11:53:00 +0530

    wip

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -162,7 +162,7 @@ async function handler(event, context) {
     }
   }
   const rand = Math.random().toString().substr(2, 8);
-  let uriTemp = "https%3A%2F%2Frailwaypgsql.onrender.com%2Fstatus%2test2s" + rand;
+  let uriTemp = "https%3A%2F%2Frailwaypgsql.onrender.com%2Fstatus/" + rand;
   console.log(uriTemp);
   url = decodeURIComponent(uriTemp);
 
