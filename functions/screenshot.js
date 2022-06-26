5a449077a0e4be9416070697aa7cbb48dc9d6500
Debjit Biswas <debjit012@gmail.com>
2022-06-26 17:21:15 +0530

    WIP

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -87,13 +87,13 @@ async function handler(event, context) {
   // e.g. /https%3A%2F%2Fwww.11ty.dev%2F/small/1:1/smaller/
   let pathSplit = event.path.split("/").filter((entry) => !!entry);
   let [url, size, aspectratio, zoom, cachebuster] = pathSplit;
-  let uriTemp = "https://wp-to-next.netlify.app/blog/wpblog/tempore-maxime-molestiae-qui-sapiente-perferendis";
+  // let uriTemp = "https://wp-to-next.netlify.app/blog/wpblog/tempore-maxime-molestiae-qui-sapiente-perferendis";
 
-  console.log(uriTemp,encodeURIComponent(uriTemp));
+  // console.log(uriTemp,encodeURIComponent(uriTemp));
 
-  if (!url) {
-      = decodeURIComponent(uriTemp);
-  }
+  // if (!url) {
+  //     = decodeURIComponent(uriTemp);
+  // }
 
 
   let format = "jpeg"; // hardcoded for now
